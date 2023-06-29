041befe93fd0a3b73b7a1f17f55d2abaa1b60a6a
Jon Ferguson <jon@jef.us>
2023-06-29 11:30:37 -0400

    missed changing the 'figures' directory for the notebook output

--- render_figures_and_notebooks.sh
+++ render_figures_and_notebooks.sh
@@ -7,7 +7,7 @@ REGIONS="Africa Asia Europe Oceania Americas Global Economies SIDS"
 mkdir -p gdsca-2022-figures/notebooks
 
 for f in $REGIONS; do
-    filename=figures/notebooks/$f
+    filename=gdsca-2022-figures/notebooks/$f
     echo "rendering $f"
     GDSCA_SINGLE_REGION="$f" jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=600 --output $filename ./GDSCA-2022.ipynb
 done
